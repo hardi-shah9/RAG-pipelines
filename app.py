61cf6a51b23b459e9814a131248674f85f8c3ccc
hardi-shah9 <harddiishah@gmail.com>
2025-11-07 22:01:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ from src.data_loader_file import load_all_documents
 
 if __name__ == "__main__":
     docs = load_all_documents("data")
-    print(f"Loaded {len(docs)} documents.")
+    # print(f"Loaded {len(docs)} documents.")
+    print(docs)
