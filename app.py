d07f5ce65a70e50659219afe069d5bc6d078a9d2
hardi-shah9 <harddiishah@gmail.com>
2025-11-12 04:16:46 +0530

    chunking
    Gave the methods for creation of chunks using chunk_document  and embed_chunks

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from src.data_loader_file import load_all_documents
+from src.embedding import EmbeddingPipeline
 
 if __name__ == "__main__":
     docs = load_all_documents("data")
     # print(f"Loaded {len(docs)} documents.")
-    print(docs)
+    # print(docs)
+    chunks = EmbeddingPipeline().chunk_documents(docs)
+    chunkvectors= EmbeddingPipeline().embed_chunks(chunks)
+    print(chunkvectors)
